58bee22be575076c622831d2b3dc9759669c2a83
Knunez2365 <Knunez2365@gmail.com>
2022-10-03 10:55:37 -0400

    refactor code

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let score = 20;
 computerGuess = Math.floor(Math.random(20) * 20);
 console.log(computerGuess);
 
-// const query = function (name) {
-//   return document.querySelector(name).textContent;
-// };
+const querySel = (qry, result) => {
+  document.querySelector(qry).textContent = result;
+};
 
 document.querySelector('.Submit').addEventListener('click', function () {
   playerGuess = parseInt(document.querySelector('.guessHuman').value);
@@ -14,32 +14,33 @@ document.querySelector('.Submit').addEventListener('click', function () {
     win = 'You win';
     document.body.style.backgroundColor = 'blue';
     newScore = score;
-    document.querySelector('.maxScore').textContent = newScore;
-    document.querySelector('.livesLeft').textContent = score;
-    document.querySelector('.guessCheck').textContent = 'YOU WIN 🤑';
-    document.querySelector('.numberComp').textContent = `${computerGuess}`;
+    querySel('.maxScore', newScore);
+    querySel('.livesLeft', score);
+    querySel('.guessCheck', 'YOU WIN! 🤑');
+    querySel('.numberComp', `${computerGuess}`);
   } else if (playerGuess > computerGuess) {
     win = 'too high';
-    document.querySelector('.livesLeft').textContent = --score;
-    document.querySelector('.guessCheck').textContent = 'Too high 🥶';
+    querySel('.livesLeft', --score);
+    querySel('.guessCheck', 'Too high 🥶');
   } else if (playerGuess < computerGuess) {
     win = 'too low';
-    document.querySelector('.livesLeft').textContent = --score;
-    document.querySelector('.guessCheck').textContent = 'Too low 🥵';
+    querySel('.livesLeft', --score);
+    querySel('.guessCheck', 'Too low 🥵');
   } else {
     win = 'no number';
-    document.querySelector('.livesLeft').textContent = --score;
-    document.querySelector('.guessCheck').textContent = 'Only numbers 👽';
+    querySel('.livesLeft', --score);
+    querySel('.guessCheck', 'Only numbers 👽');
   }
   return;
 });
 
 document.querySelector('.try').addEventListener('click', function () {
   computerGuess = Math.floor(Math.random(20) * 20);
+  console.log(computerGuess);
   score = 20;
-  document.querySelector('.livesLeft').textContent = 20;
-  document.querySelector('.numberComp').textContent = '?';
+  querySel('.livesLeft', 20);
+  querySel('.numberComp', '?');
   document.body.style.backgroundColor = 'rgb(25, 36, 36)';
-  document.querySelector('.guessCheck').textContent = 'Take a guess 🧠';
+  querySel('.guessCheck', 'Take a guess 🧠');
   document.querySelector('.guessHuman').value = '';
 });
